d92f864d5cd82f6a5ec6905663211f14c112f817
FrancoEMTG <aesevv@gmail.com>
2023-08-02 13:03:21 -0300

    arreglando bugs de sintaxis

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(parahtml))
 
 // levantar servidor en la nuve
 const port = process.env.PORT || 3001;
-app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
+app.listen(port,() =>console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, ("./views/home.html")))
