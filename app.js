b4e0a57be5ae5437aa11191a5efb58c4fc63ad2d
FrancoEMTG <aesevv@gmail.com>
2023-08-02 14:34:14 -0300

    cambiando puerto

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const parahtml = path.resolve(__dirname, ("./views"))
 app.use(express.static(parahtml))
 
 
-/* app.listen(3000, () => {
+app.listen(3001, () => {
     console.log("Servidor corriendo");
-}) */
+})
 
 // levantar servidor en la nuve
 const port = process.env.PORT || 3001;
